a6b1f825814fd730b33d88a057bf6aa96ce27324
Nicholas Herrick <nth.herrick@gmail.com>
2020-02-24 17:05:06 -0500

    added an updating message displayed throughout the game

--- src/App.js
+++ src/App.js
@@ -11,15 +11,16 @@ import './App.css';
 // Number variable start at zero
 let correctClick = 0;
 let topScore = 0;
+// Set the message to display at the begining of the game
+let message = "Dont click the same President twice!"
 
 class App extends Component {
-
   // These are the variables tracked for the app's state
   state = {
     presidents,
     correctClick,
-    topScore
-
+    topScore,
+    message
   };
 
   // This function runs whenever an image is clicked
@@ -28,16 +29,18 @@ class App extends Component {
     const presidents = this.state.presidents;
     // Set a variable to that keeps tracked of already clicked presidents
     const clickedPresident = presidents.filter(president => president.id === id);
-
     // If statement checks if a an already clicked president was clicked again
     if (clickedPresident[0].clicked) {
       // Set our score and # of correct clicks to zero
-      correctClick = 0
+      correctClick = 0;
+      // Update the game message
+      message = "Oops! That one was clicked already";
       // Loop through each president and reset them to unclicked
       presidents.map(president => president.clicked = false);
       // Reset the state of presidents and correct clicks, leaving the value of top score alone
-      this.setState({ correctClick });
-      this.setState({ presidents });
+      this.setState({correctClick});
+      this.setState({presidents});
+      this.setState({message});
       // Shuffles order of the presidents displayed
       presidents.sort(function (a, b) { return .5 - Math.random() });
       // If the click was correct, checks if the score is less than 11
@@ -47,15 +50,17 @@ class App extends Component {
       // Increment one to the score
       correctClick++;
       // Checks if the current score beat the top score
+      message = "Good, keep going";
       if (correctClick > topScore) {
         // If it did, set the new top score equal to the current score
         topScore = correctClick;
         // Update the state of top score
-        this.setState({ topScore });
+        this.setState({topScore});
       }
       // Update the rest of state variables
-      this.setState({ presidents });
-      this.setState({ correctClick });
+      this.setState({presidents});
+      this.setState({correctClick});
+      this.setState({message})
       // Shuffle the images
       presidents.sort(function (a, b) { return .5 - Math.random() });
       // This section will run if the user scores all 12 points
@@ -65,13 +70,15 @@ class App extends Component {
       correctClick = 0;
       // Max out the top score
       topScore = 12;
+      message = "Victory! You got them all!"
       // update state
       this.setState({ topScore });
       // reset all the clicked values back to false
       presidents.map(president => president.clicked = false);
       // Updates state variables
-      this.setState({ presidents });
-      this.setState({ correctClick });
+      this.setState({presidents});
+      this.setState({correctClick});
+      this.setState({message})
       // Shuffle the images
       presidents.sort(function (a, b) { return .5 - Math.random() });
     }
@@ -82,7 +89,7 @@ class App extends Component {
       <Wrapper>
         {/* Passes in state of scores to the score trackers in the nav bar as props */}
         <Nav topScore={this.state.topScore} score={this.state.correctClick} />
-        <Header>United States Presidents</Header>
+        <Header message={this.state.message}>United States Presidents</Header>
         <div className="container row mx-auto">
           {/* Maps through each president renders each one as another image card */}
           {presidents.map(president => (
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,7 @@ function Header(props) {
         <div className="text-center jumbotron">
             <h1 className="header text-center">{props.children}</h1>
             <hr/>
-            <p>Don't click on the same president more than once</p>
+            <p>{props.message}</p>
         </div>
     );
 }
